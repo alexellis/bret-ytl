1b4623a0db05b89e119bac63d2099132d0ff9fa9
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-09-17 09:59:33 +0100

    Ping CI
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- env/handler.py
+++ env/handler.py
@@ -1,7 +1,7 @@
 import os
 
 def handle(req):
-    st = "Env vars:\n"
+    st = "Env vars: - \n"
     for item, value in os.environ.items():
         st = st + '{}={}\n'.format(item, value)
     return st
