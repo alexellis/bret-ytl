e9aae0d76ee3b0c47180817928b67eb8a23e6036
Alex Ellis <alexellis2@gmail.com>
2020-09-17 09:24:16 +0100

    Update handler.py

--- env/handler.py
+++ env/handler.py
@@ -1,12 +1,7 @@
 import os
 
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
     st = ""
     for item, value in os.environ.items():
         st = st + '{}={}\n'.format(item, value)
     return st
-
