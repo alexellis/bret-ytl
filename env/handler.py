b6ff8e74386ab647a602cc2b71e91c8d4f6391e1
Alex Ellis <alexellis2@gmail.com>
2020-09-21 17:48:52 +0100

    Update handler.py

--- env/handler.py
+++ env/handler.py
@@ -1,7 +1,7 @@
 import os
 
 def handle(req):
-    st = "Env vars: - \n"
+    st = "Environment vars:\n"
     for item, value in os.environ.items():
         st = st + '{}={}\n'.format(item, value)
     return st
